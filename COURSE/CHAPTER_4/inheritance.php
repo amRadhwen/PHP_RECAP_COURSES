4c4799668deb5bb7f0f98e516d356e3816572f14
amradhwen <radhweneamri9631@gmail.com>
2024-08-10 15:51:42 +0100

    inheritance

--- COURSE/CHAPTER_4/inheritance.php
+++ COURSE/CHAPTER_4/inheritance.php
@@ -1,3 +1,34 @@
 <?php
 
     echo "<pre>";
+    
+    // when a class derives from another class
+    // child class inherit all public and protected properties and methods from the parent class
+    // inherited class is defined by using "extends" keyword
+    
+    class Fruit{
+        public $name;
+        public $color;
+        
+        public function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color;
+        }
+        
+        protected function intro() {
+            echo "The fruit is {$this->name} and the color is {$this->color}.\n";
+        }
+    }
+    
+    // Apple is inherited from Fruit
+    
+    class Apple extends Fruit {
+        public function message() {
+            echo "This is class Apple. \n";
+            $this->intro();
+        }
+    }
+    
+    $apple = new Apple("Apple", "Red");
+    $apple->message();
+    //$apple->intro();
---
+++ COURSE/CHAPTER_4/overriding.php
@@ -0,0 +1,6 @@
+<?php
+
+
+    echo "<pre>";
+    // Overriding inherited methods
+    
