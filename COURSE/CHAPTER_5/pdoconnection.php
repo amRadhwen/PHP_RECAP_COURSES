4136c2c2f7cf8d4337b56c19b37819912649b982
amradhwen <radhweneamri9631@gmail.com>
2024-08-11 00:31:50 +0100

    PDO connection

--- COURSE/CHAPTER_5/pdoconnection.php
+++ COURSE/CHAPTER_5/pdoconnection.php
@@ -1,4 +1,19 @@
 <?php
 
     echo "<pre>";
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password); // database myDB must be created before connection
+        // set PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected :)";
+    } catch (PDOException $e) {
+        echo "Connection failed: ".$e->getMessage();
+    }
+    
+    //close the connection
+    $conn = null;
 
