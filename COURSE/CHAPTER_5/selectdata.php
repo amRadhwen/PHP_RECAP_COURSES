ed4544195b241fc2a2ecb9169803817a072613a2
amradhwen <radhweneamri9631@gmail.com>
2024-08-11 23:07:43 +0100

    php mysq select data

--- COURSE/CHAPTER_5/selectdata.php
+++ COURSE/CHAPTER_5/selectdata.php
@@ -1,3 +1,11 @@
+<style type="text/css">
+    table {
+        border-collapse: collapse;
+    }
+    table  td, table th{
+        border: 1px solid black;
+    }
+</style>
 <?php
 
     echo "<pre>";
@@ -9,7 +17,17 @@
     if($dbconncetion->connect()) {
         try {
             $stmt = $dbconncetion->getConnection()->prepare("SELECT id, firstname, lastname, email FROM Guests");
-            var_dump($stmt->execute());
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            //var_dump($result);
+            echo "<table><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+            foreach($result as $key => $arr) {
+                echo "<tr><td>".$arr["id"]."</td><td>".$arr["firstname"]."</td><td>".$arr["lastname"]."</td><td>".$arr["email"]."</td></tr>";
+            }
+            echo "</table>";
+            
+            
         } catch (PDOException $ex) {
             echo $ex->getMessage();
         }
